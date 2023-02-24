c41a9aefaaf12e7a35e0076e086e0b40626ed990
L.Gsell.inf21 <l.gsell.inf21@stud.bbbaden.ch>
2023-02-24 09:01:21 +0100

    Added Firebase SDK

---
+++ password-golem/src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBklVCoD5bv5jt0OeWzdqFITbOd5X8PlsU',
+  authDomain: 'password-manager-8b8d7.firebaseapp.com',
+  projectId: 'password-manager-8b8d7',
+  storageBucket: 'password-manager-8b8d7.appspot.com',
+  messagingSenderId: '615249213289',
+  appId: '1:615249213289:web:1fdf678771aa874b75eff8',
+}
+
+const app = initializeApp(firebaseConfig)
